b6d14d98158c3b55d3421ec4eacad6ffd8a2d231
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-07 00:09:47 +1000

    Add files via upload

---
+++ src/builder/world/World.java
@@ -0,0 +1,11 @@
+package builder.world;
+
+import builder.entities.tiles.Tile;
+import engine.renderer.Dimensions;
+import java.util.List;
+
+public interface World {
+    List<Tile> tilesAtPosition(int x, int y, Dimensions dimensions);
+    List<Tile> allTiles();
+    void place(Tile tile);
+}
