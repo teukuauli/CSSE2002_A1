45ce58a78038d835cd6c7fa3300d5001512fd72a
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-12 00:47:08 +1000

    Update Interactable.java

--- src/builder/entities/Interactable.java
+++ src/builder/entities/Interactable.java
@@ -3,6 +3,17 @@ package builder.entities;
 import builder.GameState;
 import engine.EngineState;
 
+/**
+ * Represents an entity that can be interacted with in the game.
+ * Classes implementing this interface should define what happens
+ * when the entity is interacted with.
+ */
 public interface Interactable {
+    /**
+     * Defines the action to be performed when the entity is interacted with.
+     *
+     * @param state the current state of the engine
+     * @param game  the current state of the game
+     */
     void interact(EngineState state, GameState game);
 }
