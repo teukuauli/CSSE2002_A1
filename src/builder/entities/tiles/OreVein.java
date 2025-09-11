40775a254c864f2082076fbcbf49a1b6e1102398
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-12 00:57:09 +1000

    Update OreVein.java

--- src/builder/entities/tiles/OreVein.java
+++ src/builder/entities/tiles/OreVein.java
@@ -3,15 +3,30 @@ package builder.entities.tiles;
 import builder.ui.SpriteGallery;
 import builder.entities.resources.Ore;
 
+/**
+ * Represents an ore vein tile that contains an Ore resource.
+ */
 public class OreVein extends Tile {
     private Ore ore;
 
+    /**
+     * Constructs an ore vein tile at the given position and places an Ore on it.
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     */
     public OreVein(int x, int y) {
         super(x, y, SpriteGallery.field);
-        this.ore = new Ore(x, y);  // Add coordinates
+        this.ore = new Ore(x, y);  
         placeOn(ore);
     }
-    public Ore getOre(){
+
+    /**
+     * Gets the Ore resource contained in this vein.
+     *
+     * @return the ore entity
+     */
+    public Ore getOre() {
         return ore;
     }
 }
