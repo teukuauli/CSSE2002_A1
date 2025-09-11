409bd835460ab8074143c98fcc7c155a5c488508
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-12 00:51:36 +1000

    Update Ore.java

--- src/builder/entities/resources/Ore.java
+++ src/builder/entities/resources/Ore.java
@@ -8,11 +8,21 @@ import engine.EngineState;
 import engine.game.Entity;
 import engine.game.HasTick;
 
+/**
+ * Represents an ore resource in the game that can be mined with a jackhammer.
+ * The ore depletes over time as it is mined and provides coins to the player.
+ */
 public class Ore extends Entity implements Usable, HasTick {
     private int value = 10;
     private final int originalValue = 10;
     private int tickCounter = 0;
 
+    /**
+     * Constructs an ore entity at the given position.
+     *
+     * @param x the x-coordinate of the ore
+     * @param y the y-coordinate of the ore
+     */
     public Ore(int x, int y) {
         super(x, y);
         setSprite(SpriteGallery.rock.getSprite("default"));
