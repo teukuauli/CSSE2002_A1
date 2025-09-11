866a825fb5f7eb4dbcc6807057d4d01a08de050e
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-12 01:11:13 +1000

    Update Player.java

--- src/builder/player/Player.java
+++ src/builder/player/Player.java
@@ -1,7 +1,29 @@
 package builder.player;
 
+/**
+ * Represents a player character in the game.
+ * Provides access to position and base damage.
+ */
 public interface Player {
+
+    /**
+     * Gets the player's current x-coordinate.
+     *
+     * @return the x-coordinate
+     */
     int getX();
+
+    /**
+     * Gets the player's current y-coordinate.
+     *
+     * @return the y-coordinate
+     */
     int getY();
+
+    /**
+     * Gets the base damage value dealt by the player.
+     *
+     * @return the damage value
+     */
     int getDamage();
 }
