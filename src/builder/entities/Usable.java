40491e642c81b446a301f6f075a31afc775d022b
teukuauli <112930506+teukuauli@users.noreply.github.com>
2025-09-12 00:48:18 +1000

    Update Usable.java

--- src/builder/entities/Usable.java
+++ src/builder/entities/Usable.java
@@ -3,6 +3,16 @@ package builder.entities;
 import builder.GameState;
 import engine.EngineState;
 
+/**
+ * Represents an entity in the game that can be used.
+ * Classes implementing this interface should define how the entity is used.
+ */
 public interface Usable {
+    /**
+     * Defines the behavior when this entity is used.
+     *
+     * @param state the current engine state
+     * @param game  the current game state
+     */
     void use(EngineState state, GameState game);
 }
